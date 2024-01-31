18909c443971ce40c869c90637c1f932f37aba8b
Aru Hanazawa <williwilliam4@gmail.com>
2024-01-31 22:14:43 +0700

    edit by Alby

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,1 +1,17 @@
-ini halaman dashboard
+@extends('layout.main')
+
+@section('content')
+<div class="container-xxl">
+    <div class="row">
+        <div class="col">
+            {{-- sidebar --}}
+            @include('additional.sidebar')
+            {{-- sidebar --}}
+        </div>
+        <div class="col">
+            ini bagian isinya entar
+        </div>
+    </div>
+</div>
+
+@endsection
