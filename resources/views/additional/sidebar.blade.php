dc2327e489bd4ca93113263de18fdcc3d476ecbc
Aru Hanazawa <williwilliam4@gmail.com>
2024-02-01 23:34:32 +0700

    edit by Alby

--- resources/views/additional/sidebar.blade.php
+++ resources/views/additional/sidebar.blade.php
@@ -1,28 +1,25 @@
-<div class="d-flex flex-column p-3 border-end" style="width: 280px; min-height: 100vh;">
-    <a href="{{ url('dashboard') }}" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto link-body-emphasis text-decoration-none">
-        <img src="{{ asset('img/logo_hma.png') }}" alt="logo hanatekindo">
+<div class="d-flex flex-column border-end" style="width: 280px; min-height: 100vh;">
+    <a href="{{ url('dashboard') }}" class="d-flex justify-content-center align-items-center mt-3 ms-2 link-body-emphasis text-decoration-none">
+        <img src="{{ asset('img/logo_hma.png') }}" style="width: 40px" alt="logo hanatekindo">
         <span class="fs-4 fw-bolder" style="color: rgb(151, 23, 23)">PT. Hanatekindo</span>
     </a>
     <hr>
-    <ul class="nav nav-pills flex-column mb-auto">
-        <li class="nav-item mt-5">
-            <a href="{{ url('dashboard') }}" class="nav-link  {{ ($ht === "Dashboard") ? 'active' : '' }}">
-                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="me-2 bi bi-speedometer" viewBox="0 0 16 16"><path d="M8 2a.5.5 0 0 1 .5.5V4a.5.5 0 0 1-1 0V2.5A.5.5 0 0 1 8 2M3.732 3.732a.5.5 0 0 1 .707 0l.915.914a.5.5 0 1 1-.708.708l-.914-.915a.5.5 0 0 1 0-.707M2 8a.5.5 0 0 1 .5-.5h1.586a.5.5 0 0 1 0 1H2.5A.5.5 0 0 1 2 8m9.5 0a.5.5 0 0 1 .5-.5h1.5a.5.5 0 0 1 0 1H12a.5.5 0 0 1-.5-.5m.754-4.246a.39.39 0 0 0-.527-.02L7.547 7.31A.91.91 0 1 0 8.85 8.569l3.434-4.297a.39.39 0 0 0-.029-.518z"/><path fill-rule="evenodd" d="M6.664 15.889A8 8 0 1 1 9.336.11a8 8 0 0 1-2.672 15.78zm-4.665-4.283A11.95 11.95 0 0 1 8 10c2.186 0 4.236.585 6.001 1.606a7 7 0 1 0-12.002 0"/>
-                </svg>
+    <ul class="nav nav-pills flex-column p-2 mb-auto" id="inav">
+        <li class="nav-item mt-5" id="nav">
+            <a href="{{ url('dashboard') }}" id="a" class="nav-link link-body-emphasis {{ ($ht === "Dashboard") ? 'active' : '' }}">
+                <i class="bi bi-house-fill"></i>
                 Dashboard
             </a>
         </li>
         <li class="mt-2">
-            <a href="{{ url('master') }}" class="nav-link {{ ($ht === "Master Pengguna") ? 'active' : '' }}">
-                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="me-2 bi bi-person-circle" viewBox="0 0 16 16"><path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0"/><path fill-rule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8m8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1"/>
-                </svg>
+            <a href="{{ url('master') }}" class="nav-link link-body-emphasis {{ ($ht === "Master Pengguna") ? 'active' : '' }}">
+                <i class="bi bi-person-circle"></i>
                 Master Pengguna
             </a>
         </li>
         <li class="my-5">
             <a href="#" class="nav-link link-body-emphasis">
-                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="me-2 bi bi-box-arrow-left" viewBox="0 0 16 16"><path fill-rule="evenodd" d="M6 12.5a.5.5 0 0 0 .5.5h8a.5.5 0 0 0 .5-.5v-9a.5.5 0 0 0-.5-.5h-8a.5.5 0 0 0-.5.5v2a.5.5 0 0 1-1 0v-2A1.5 1.5 0 0 1 6.5 2h8A1.5 1.5 0 0 1 16 3.5v9a1.5 1.5 0 0 1-1.5 1.5h-8A1.5 1.5 0 0 1 5 12.5v-2a.5.5 0 0 1 1 0z"/><path fill-rule="evenodd" d="M.146 8.354a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L1.707 7.5H10.5a.5.5 0 0 1 0 1H1.707l2.147 2.146a.5.5 0 0 1-.708.708z"/>
-                </svg>
+                <i class="bi bi-box-arrow-left"></i>
                 Logout
             </a>
         </li>
--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -3,16 +3,48 @@
 @section('content')
 <div class="container-fluid">
     <div class="row">
-        <div class="sidebar col-md-3">
+        <div class="sidebar" style="width: 21%">
             {{-- sidebar --}}
             @include('additional.sidebar')
             {{-- sidebar --}}
         </div>
-        <main class="col-md-9 col-lg-10 px-md-4">
-            <div class="col">
-                <div class="d-flex flex-row position-absolute top-50 start-50 translate-middle ms-5">
-                    table
+        <main class="col-md-9">
+            <div class="border-bottom d-flex align-items-center mb-3 link-body-emphasis text-decoration-none">
+                head
+            </div>
+            <div class="col mt-5 ms-4">
+                <div class="row">
+                    <div class="col">
+                        <a href="" class="btn btn-primary mb-3"><i class="bi bi-plus-circle"></i> Tambah</a>
+                    </div>
+                    <div class="col">
+                        <div class="input-group mb-3">
+                            <span class="input-group-text" id="basic-addon1"><i class="bi bi-search"></i></span>
+                            <input type="text" class="form-control" placeholder="Search" aria-label="Username" aria-describedby="basic-addon1">
+                        </div>
+                    </div>
                 </div>
+                <table class="table table-bordered table-hover">
+                    <thead>
+                        <tr>
+                            <th >No</th>
+                            <th style="width: 40%">Nama Pengguna</th>
+                            <th style="width: 40%">Email</th>
+                            <th >Aksi</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <th scope="row">1</th>
+                            <td>Albiagi</td>
+                            <td>albyagiw@gmail.com</td>
+                            <td>
+                                <button class="btn btn-warning btn-sm"><i class="bi bi-pencil-square"></i></button>
+                                <button class="btn btn-danger btn-sm"><i class="bi bi-trash"></i></button>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
             </div>
         </main>
     </div>
